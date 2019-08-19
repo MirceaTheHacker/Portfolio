408a1950efb90b877cd653c1e1cab12db8901a50
MirceaMatei <matei_mircea97@yahoo.com>
2019-08-19 18:23:15 +0300

    implemented vertical scroll

--- src/App.js
+++ src/App.js
@@ -6,75 +6,71 @@ import { Link, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <div className="th">
-        <Layout>
-          <Header
-            className="header-color"
-            title={
-              <Link
-                className="nav-bar"
-                style={{
-                  textDecoration: "none",
-                  color: "white",
-                  fontSize: "2em"
-                }}
-                to="/"
-              >
-                My portfolio
-              </Link>
-            }
-            scroll
-          >
-            <Navigation>
-              <Link
-                basename={process.env.PUBLIC_URL}
-                to="/resume"
-                className="nav-bar"
-                style={{ fontSize: "2em" }}
-              >
-                Resume
-              </Link>
-              {/* <Link to="/aboutme" className="nav-bar" style={{ fontSize: "2em" }}>
+    // <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <div>
+      <Layout className="body">
+        <Header
+          className="header-color"
+          title={
+            <Link
+              className="nav-bar"
+              style={{
+                textDecoration: "none",
+                color: "white",
+                fontSize: "2em"
+              }}
+              to="/"
+            >
+              My portfolio
+            </Link>
+          }
+          scroll
+        >
+          <Navigation>
+            <Link
+              // basename={process.env.PUBLIC_URL}
+              to="/resume"
+              className="nav-bar"
+              style={{ fontSize: "2em" }}
+            >
+              Resume
+            </Link>
+            {/* <Link to="/aboutme" className="nav-bar" style={{ fontSize: "2em" }}>
               About Me
             </Link> */}
-              <Link
-                to="/projects"
-                className="nav-bar"
-                style={{ fontSize: "2em" }}
-              >
-                Projects
-              </Link>
-              <Link
-                to="/contact"
-                className="nav-bar"
-                style={{ fontSize: "2em" }}
-              >
-                Contact
-              </Link>
-            </Navigation>
-          </Header>
-          <Drawer
-            title={
-              <Link style={{ textDecoration: "none", color: "black" }} to="/">
-                My portfolio
-              </Link>
-            }
-          >
-            <Navigation>
-              <Link to="/resume">Resume</Link>
-              {/* <Link to="/aboutme">About Me</Link> */}
-              <Link to="/projects">Projects</Link>
-              <Link to="/contact">Contact</Link>
-            </Navigation>
-          </Drawer>
-          <Content>
-            <div className="page-content" />
-            <Main />
-          </Content>
-        </Layout>
-      </div>
-    </BrowserRouter>
+            <Link
+              to="/projects"
+              className="nav-bar"
+              style={{ fontSize: "2em" }}
+            >
+              Projects
+            </Link>
+            <Link to="/contact" className="nav-bar" style={{ fontSize: "2em" }}>
+              Contact
+            </Link>
+          </Navigation>
+        </Header>
+        <Drawer
+          title={
+            <Link style={{ textDecoration: "none", color: "black" }} to="/">
+              My portfolio
+            </Link>
+          }
+        >
+          <Navigation>
+            <Link to="/resume">Resume</Link>
+            {/* <Link to="/aboutme">About Me</Link> */}
+            <Link to="/projects">Projects</Link>
+            <Link to="/contact">Contact</Link>
+          </Navigation>
+        </Drawer>
+        <Content>
+          <div className="page-content" />
+          <Main />
+        </Content>
+      </Layout>
+    </div>
+    // </BrowserRouter>
   );
 }
 
