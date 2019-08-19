bd02d81d5703c5d4ab06db656da1478ae934fd18
MirceaMatei <matei_mircea97@yahoo.com>
2019-08-19 17:47:21 +0300

    Adding horizontal scroll

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Layout, Header, Navigation, Content, Drawer } from "react-mdl";
+import Main from "./components/main";
+import { Link, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <div className="th">
+        <Layout>
+          <Header
+            className="header-color"
+            title={
+              <Link
+                className="nav-bar"
+                style={{
+                  textDecoration: "none",
+                  color: "white",
+                  fontSize: "2em"
+                }}
+                to="/"
+              >
+                My portfolio
+              </Link>
+            }
+            scroll
+          >
+            <Navigation>
+              <Link
+                basename={process.env.PUBLIC_URL}
+                to="/resume"
+                className="nav-bar"
+                style={{ fontSize: "2em" }}
+              >
+                Resume
+              </Link>
+              {/* <Link to="/aboutme" className="nav-bar" style={{ fontSize: "2em" }}>
+              About Me
+            </Link> */}
+              <Link
+                to="/projects"
+                className="nav-bar"
+                style={{ fontSize: "2em" }}
+              >
+                Projects
+              </Link>
+              <Link
+                to="/contact"
+                className="nav-bar"
+                style={{ fontSize: "2em" }}
+              >
+                Contact
+              </Link>
+            </Navigation>
+          </Header>
+          <Drawer
+            title={
+              <Link style={{ textDecoration: "none", color: "black" }} to="/">
+                My portfolio
+              </Link>
+            }
+          >
+            <Navigation>
+              <Link to="/resume">Resume</Link>
+              {/* <Link to="/aboutme">About Me</Link> */}
+              <Link to="/projects">Projects</Link>
+              <Link to="/contact">Contact</Link>
+            </Navigation>
+          </Drawer>
+          <Content>
+            <div className="page-content" />
+            <Main />
+          </Content>
+        </Layout>
+      </div>
+    </BrowserRouter>
   );
 }
 
