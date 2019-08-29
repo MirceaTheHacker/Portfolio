b1aa02e42cf2472dd92635052afd9a3f93b592ab
Matei Mircea <37453637+MirceaTheHacker@users.noreply.github.com>
2019-08-29 19:18:32 +0300

    basenaming in app.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import "react-mdl/extra/material.js";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/Portfolio">
     <App />
   </BrowserRouter>,
   document.getElementById("root")
